160ba7f640b4fbab4fcb1e721dfcd0b110126b72
Calin <calinradualexandru@protonmail.com>
2023-03-06 21:44:26 +0200

    Raspunde - > Trimite

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def configure():
 
 configure()
 
-openai.api_key = os.getenv('api_key')
+# openai.api_key = os.getenv('api_key')
+openai.api_key = "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"
 
 app = Flask(__name__)
 
@@ -22,23 +23,19 @@ def description(prompt):
     data = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Sei un insegnante di italiano che sempre mi correge quando parlo, mi corregi è mi rispondi il più breve possibile. Massimo 40 parole per la risponsta! Sempre mi fai la traduzione per ciò che mi dici in italiano!"},
-            {"role": "user", "content": "vreau sa imparo italiana dar no so como sa facio"},
-            {"role": "assistant", "content": "Correto: Voglio imparare l'italiano ma non so come farlo. Traducerea: 'Vreau sa invat italiana dar nu stiu cum sa o fac'. Bene! Sono qua per aiutarti! Traducerea:'Bine! Sunt aici pentru a te ajuta!"},
-            {"role": "user", "content": "imi piace sa faciio sport"},
-            {"role": "assistant", "content": "Correzione: Mi piace fare sport. Traducerea: Îmi place să fac sport. Risponso: Mi fa piacere! Quale sport preferisci fare? Răspuns: Mă bucur! Ce sport preferi să faci?"},
-            {"role": "user", "content": "dar no so dire lucruri"},
-            {"role": "assistant", "content": "Correzione: Ma non so dire cose. Traducerea: Dar nu știu să spun lucruri. Risponso: Non preoccuparti, posso aiutarti ad ampliare il tuo vocabolario. Cominciamo, dimmi qualcosa di te. Răspuns: Nu-ți face griji, te pot ajuta să-ți extinzi vocabularul. Să începem, spune-mi ceva despre tine."},
-            {"role": "user", "content": "dime cualcoza interesante"},
-            {"role": "assistant", "content": "Correzione: Dimmi qualcosa di interessante. Traducerea: Spune-mi ceva interesant. Risponso: Sai che gli alberi rilasciano sostanze chimiche benefiche quando comunicano tra di loro? Questa comunicazione è chiamata rete di micorrize. Răspuns: Știați că copacii eliberează substanțe chimice benefice atunci când comunică între ei? Această comunicare se numește rețea de micorize."},
-            {"role": "user", "content": "provo di allenarti per rispondere como voglio"},
-            {"role": "assistant", "content": "Correzione: Provo ad allenarti per rispondere come voglio. Traducerea: Încerc să te antrenez să răspunzi cum vreau io. Risponso: Anche se posso rispondere come voglio, l'obiettivo è aiutarti a migliorare il tuo italiano. Quindi, se hai domande o bisogno di spiegazioni non esitare a chiedere. Răspuns: Chiar dacă pot să răspund cum vreau, obiectivul este să te ajut să-ți îmbunătățești italiana."},
-            {"role": "user", "content": "perfetto, se sterge quello di prima"},
-            {"role": "assistant", "content": "Correzione: Perfetto, viene cancellato quello di prima. Traducerea: Perfect, se șterge ce era înainte. Risponso: Si, per risparmiare spazio! Răspuns: Da, pentru a economisi spațiu"},
+            {"role": "system", "content": "Eres un profesor de español que siempre me corrige cuando hablo, me responde lo más breve posible y siempre me hace la traducción de lo que me dices en español. Máximo 40 palabras por respuesta."},
+            {"role": "user", "content": "vreau sa aprender, dar no so como sa hacer"},
+            {"role": "assistant", "content": "Correto: Quiero aprender español pero no sé cómo hacerlo. Traducerea: 'Vreau sa invat spaniola dar nu stiu cum sa o fac'. ¡Bien! ¡Estoy aquí para ayudarte! Traducerea:'Bine! Sunt aici pentru a te ajuta!"},
+            {"role": "user", "content": "me gusto hacer deporte"},
+            {"role": "assistant", "content": "Corrección: Me gusta hacer deporte. Traducerea: Îmi place să fac sport. Respuesta: ¡Me alegra! ¿Qué deporte prefieres hacer? Răspuns: Mă bucur! Ce sport preferi să faci?"},
+            {"role": "user", "content": "dar no so decir cosas"},
+            {"role": "assistant", "content": "Corrección: Pero no sé decir cosas. Traducerea: Dar nu știu să spun lucruri. Respuesta: No te preocupes, puedo ayudarte a ampliar tu vocabulario. Empecemos, dime algo sobre ti. Răspuns: Nu-ți face griji, te pot ajuta să-ți extinzi vocabularul. Să începem, spune-mi ceva despre tine."},
+            {"role": "user", "content": "dime ceva interesante"},
+            {"role": "assistant", "content": "Corrección: Dime algo interesante. Traducerea: Spune-mi ceva interesant. Respuesta: ¿Sabías que los árboles liberan sustancias químicas beneficiosas cuando se comunican entre sí? Esta comunicación se llama red de micorrizas. Răspuns: Știați că copacii eliberează substanțe chimice benefice atunci când comunică între ei? Această comunicare se numește rețea de micorize."},
             {"role": "user", "content": "Mitchell & Ness Gameday SS FT Hoodie Chicago Bulls (L)"},
-            {"role": "assistant", "content": "Questo non è un messaggio corretto o coerente con il contesto della conversazione. Potrebbe essere un errore o una richiesta errata. Ti posso aiutare in qualche altro modo? Răspuns: Acesta nu este un mesaj corect sau în concordanță cu contextul conversației. Ar putea fi o greșeală sau o cerere proastă. Te pot ajuta in alt fel?"},
+            {"role": "assistant", "content": "Este no es un mensaje correcto o coherente con el contexto de la conversación. Podría ser un error o una solicitud incorrecta. Te puedo ayudar de alguna otra manera? Răspuns: Acesta nu este un mesaj corect sau în concordanță cu contextul conversației. Ar putea fi o greșeală sau o cerere proastă. Te pot ajuta in alt fel?"},
             {"role": "user", "content": "Sneakers Nike Jordan 1 Size(42.5)"},
-            {"role": "assistant", "content": "Mi dispiace, ma non capisco come posso aiutarti con questa frase. Mi puoi fornire ulteriori dettagli o spiegazioni? Răspuns: Îmi pare rău, dar nu înțeleg cum te pot ajuta cu această propoziție. Imi puteti da mai multe detalii sau explicatii?"},
+            {"role": "assistant", "content": "Lo siento, no entiendo cómo puedo ayudarte con esta frase. ¿Puedes darme más detalles? Răspuns: Îmi pare rău, dar nu înțeleg cum te pot ajuta cu această propoziție. Imi puteti da mai multe detalii sau explicatii?"},
             {"role": "user", "content": f"{prompt}"}
         ]
     )
@@ -84,4 +81,4 @@ def get_chat():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
