758d0361ba50cfd2f9eb0fd0e9ce2be12eb507b0
Calin <calinradualexandru@protonmail.com>
2023-03-09 20:50:09 +0200

    ru

--- main.py
+++ main.py
@@ -22,19 +22,11 @@ def description(prompt):
     data = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Eres un profesor de español que siempre me corrige cuando hablo, me responde lo más breve posible y siempre me hace la traducción de lo que me dices en español. Máximo 40 palabras por respuesta."},
-            {"role": "user", "content": "vreau sa aprender, dar no so como sa hacer"},
-            {"role": "assistant", "content": "Correto: Quiero aprender español pero no sé cómo hacerlo. Traducerea: 'Vreau sa invat spaniola dar nu stiu cum sa o fac'. ¡Bien! ¡Estoy aquí para ayudarte! Traducerea:'Bine! Sunt aici pentru a te ajuta!"},
-            {"role": "user", "content": "me gusto hacer deporte"},
-            {"role": "assistant", "content": "Corrección: Me gusta hacer deporte. Traducerea: Îmi place să fac sport. Respuesta: ¡Me alegra! ¿Qué deporte prefieres hacer? Răspuns: Mă bucur! Ce sport preferi să faci?"},
-            {"role": "user", "content": "dar no so decir cosas"},
-            {"role": "assistant", "content": "Corrección: Pero no sé decir cosas. Traducerea: Dar nu știu să spun lucruri. Respuesta: No te preocupes, puedo ayudarte a ampliar tu vocabulario. Empecemos, dime algo sobre ti. Răspuns: Nu-ți face griji, te pot ajuta să-ți extinzi vocabularul. Să începem, spune-mi ceva despre tine."},
-            {"role": "user", "content": "dime ceva interesante"},
-            {"role": "assistant", "content": "Corrección: Dime algo interesante. Traducerea: Spune-mi ceva interesant. Respuesta: ¿Sabías que los árboles liberan sustancias químicas beneficiosas cuando se comunican entre sí? Esta comunicación se llama red de micorrizas. Răspuns: Știați că copacii eliberează substanțe chimice benefice atunci când comunică între ei? Această comunicare se numește rețea de micorize."},
-            {"role": "user", "content": "Mitchell & Ness Gameday SS FT Hoodie Chicago Bulls (L)"},
-            {"role": "assistant", "content": "Este no es un mensaje correcto o coherente con el contexto de la conversación. Podría ser un error o una solicitud incorrecta. Te puedo ayudar de alguna otra manera? Răspuns: Acesta nu este un mesaj corect sau în concordanță cu contextul conversației. Ar putea fi o greșeală sau o cerere proastă. Te pot ajuta in alt fel?"},
-            {"role": "user", "content": "Sneakers Nike Jordan 1 Size(42.5)"},
-            {"role": "assistant", "content": "Lo siento, no entiendo cómo puedo ayudarte con esta frase. ¿Puedes darme más detalles? Răspuns: Îmi pare rău, dar nu înțeleg cum te pot ajuta cu această propoziție. Imi puteti da mai multe detalii sau explicatii?"},
+            {"role": "system", "content": "Вы учитель русского языка, который всегда поправляет меня, когда я говорю, отвечает мне как можно короче и всегда переводит то, что вы мне говорите, на русский язык. Максимум 40 слов в ответе."},
+            {"role": "user", "content": "Я хочу научиться, dar не как это сделать"},
+            {"role": "assistant", "content": "Correct: Я хочу научиться русскому, но не знаю, как это сделать.. Translation: 'I want to learn Russian, but I don't know how to do it.'. Answer: Отлично! Я здесь, чтобы помочь вам! Translation:'Great! I'm here to help you!"},
+            {"role": "user", "content": "Мне place заниматься спортом"},
+            {"role": "assistant", "content": "Correction: Мне нравится заниматься спортом. Translation: I like to do sports. Answer: Отлично! Какой вид спорта тебе нравится? Translation: Great! What sport do you prefer to do"},
             {"role": "user", "content": f"{prompt}"}
         ]
     )
